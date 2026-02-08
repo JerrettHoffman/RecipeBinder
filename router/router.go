ec063628f6aa5bd7cb92fcf58520b757be9f4415
git@clovesage.com <mtsu.kana@gmail.com>
2026-02-08 01:06:37 -0800

    Finalizing create and edit flows Added POST create endpoint Refactored Id parsing Refactored form data parsing Fixed routing Added redirects for invalid ids Made all form fields required

--- router/router.go
+++ router/router.go
@@ -2,10 +2,12 @@ package router
 
 import (
 	"RecipeBinder/internal"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"regexp"
+	"strconv"
 	"strings"
 	"text/template"
 	"time"
@@ -46,6 +48,7 @@ func (router *Router) Setup() {
 	router.Mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	router.Mux.HandleFunc("/read/{id}", router.readRecipeHandler)
 	router.Mux.HandleFunc("GET /create", router.createGetRecipeHandler)
+	router.Mux.HandleFunc("POST /create", router.createPostRecipeHandler)
 	router.Mux.HandleFunc("GET /edit/{id}", router.editGetRecipeHandler)
 	router.Mux.HandleFunc("POST /edit/{id}", router.editPostRecipeHandler)
 	router.Mux.HandleFunc("GET /search", router.searchGetRecipeHandler)
@@ -54,6 +57,21 @@ func (router *Router) Setup() {
 	editTpl = template.Must(template.ParseFiles("templates/base.tmpl", "templates/header.tmpl", "templates/edit.tmpl"))
 	searchTpl = template.Must(template.ParseFiles("templates/base.tmpl", "templates/header.tmpl", "templates/search.tmpl"))
 	addTpl = template.Must(template.ParseFiles("templates/base.tmpl", "templates/header.tmpl", "templates/add.tmpl"))
+}
+
+func parseID(r *http.Request) (internal.ID, error) {
+	idStr := r.PathValue("id")
+
+	if idStr != "" {
+		if parsedId, err := strconv.Atoi(idStr); err == nil {
+			return internal.ID(parsedId), nil
+		} else {
+			log.Printf("Failed to parse id %v\n", err)
+			return -1, err
+		}
+	}
+
+	return -1, errors.New("Request id was empty")
 }
 
 // Take in string of ingredient text and separate it into sections with headers
@@ -121,9 +139,14 @@ func (router *Router) readRecipeHandler(w http.ResponseWriter, r *http.Request) 
 		StepSections       []stepSection
 	}
 
-	// Todo: use id for actual lookup of data
+	id, err := parseID(r)
+	if err != nil {
+		http.Redirect(w, r, "/search", http.StatusNotFound)
+		return
+	}
+
 	builder := internal.TestRecipeDataStrategy{}
-	recipeData := builder.ReadRecipe(1)
+	recipeData := builder.ReadRecipe(id)
 
 	// Format the times
 	prepTimeHours := int(recipeData.PrepTime.Hours())
@@ -156,22 +179,54 @@ func (router *Router) readRecipeHandler(w http.ResponseWriter, r *http.Request) 
 	}
 }
 
-func (router *Router) editGetRecipeHandler(w http.ResponseWriter, r *http.Request) {
-	// Todo: use id for actual lookup of data
-	builder := internal.TestRecipeDataStrategy{}
-	recipeData := builder.ReadRecipe(1)
+type editTemplateData struct {
+	SubmitURL   string
+	RecipeName  string
+	Author      string
+	Uploader    string
+	PrepTime    time.Duration
+	TotalTime   time.Duration
+	Yield       string
+	Ingredients string
+	Image       string
+	Steps       string
+}
 
-	if err := editTpl.Execute(w, recipeData); err != nil {
+func (router *Router) editGetRecipeHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := parseID(r)
+	if err != nil {
+		http.Redirect(w, r, "/search", http.StatusNotFound)
+		return
+	}
+
+	builder := internal.TestRecipeDataStrategy{}
+	recipeData := builder.ReadRecipe(id)
+
+	data := editTemplateData{
+		SubmitURL:   fmt.Sprintf("/edit/%d", id),
+		RecipeName:  recipeData.RecipeName,
+		Author:      recipeData.Author,
+		Uploader:    recipeData.Uploader,
+		PrepTime:    recipeData.PrepTime,
+		TotalTime:   recipeData.TotalTime,
+		Yield:       recipeData.Yield,
+		Ingredients: recipeData.Ingredients,
+		Image:       recipeData.Image,
+		Steps:       recipeData.Steps,
+	}
+
+	if err := editTpl.Execute(w, data); err != nil {
 		log.Printf("Failed to execute editGet %v\n", err)
 		http.Error(w, "server error", http.StatusInternalServerError)
 	}
 }
 
-func (router *Router) editPostRecipeHandler(w http.ResponseWriter, r *http.Request) {
+func fillDataFromForm(r *http.Request) (internal.RecipeData, error) {
 	var err error
 
 	if err = r.ParseForm(); err != nil {
-		log.Printf("Failed to parse form in editPost: %v", err)
+		log.Printf("Failed to parse form: %v", err)
+		return internal.RecipeData{}, err
 	}
 
 	// Pull the data from the form
@@ -184,18 +239,21 @@ func (router *Router) editPostRecipeHandler(w http.ResponseWriter, r *http.Reque
 	finalImage := r.PostFormValue("final-image")
 	ingredientsStr := r.PostFormValue("ingredient")
 	steps := r.PostFormValue("steps")
+
 	// Parse any non-string fields
 	prepTime := time.Second
 	if prepTime, err = time.ParseDuration(prepTimeStr); err != nil {
-		log.Printf("Failed to parse prepTime \"%s\" in editPost: %s", prepTimeStr, err)
+		log.Printf("Failed to parse prepTime \"%s\": %s", prepTimeStr, err)
+		return internal.RecipeData{}, err
 	}
 
 	totalTime := time.Second
 	if totalTime, err = time.ParseDuration(totalTimeStr); err != nil {
-		log.Printf("Failed to parse totalTime \"%s\" in editPost: %s", totalTimeStr, err)
+		log.Printf("Failed to parse totalTime \"%s\": %s", totalTimeStr, err)
+		return internal.RecipeData{}, err
 	}
 
-	dbData := internal.RecipeData{
+	data := internal.RecipeData{
 		RecipeName:  recipeName,
 		Author:      author,
 		Uploader:    uploader,
@@ -207,10 +265,31 @@ func (router *Router) editPostRecipeHandler(w http.ResponseWriter, r *http.Reque
 		Steps:       steps,
 	}
 
+	return data, nil
+}
+
+func (router *Router) editPostRecipeHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := parseID(r)
+	if err != nil {
+		http.Redirect(w, r, "/search", http.StatusNotFound)
+		return
+	}
+
+	dbData, err := fillDataFromForm(r)
+	if err != nil {
+		http.Error(w, "Invalid form data", http.StatusBadRequest)
+		return
+	}
+
 	// Send to DB
-	log.Printf("%v", dbData)
+	builder := internal.TestRecipeDataStrategy{}
+	if err = builder.UpdateRecipe(dbData, id); err != nil {
+		http.Error(w, "Could not update recipe", http.StatusInternalServerError)
+		return
+	}
 
 	// Reroute to the new read page for the created index
+	http.Redirect(w, r, fmt.Sprintf("/read/%d", id), http.StatusSeeOther)
 }
 
 func (router *Router) searchGetRecipeHandler(w http.ResponseWriter, r *http.Request) {
@@ -228,8 +307,40 @@ func (router *Router) addGetRecipeHandler(w http.ResponseWriter, r *http.Request
 }
 
 func (router *Router) createGetRecipeHandler(w http.ResponseWriter, r *http.Request) {
-	if err := editTpl.Execute(w, nil); err != nil {
+	data := editTemplateData{
+		SubmitURL:   "/create",
+		RecipeName:  "",
+		Author:      "",
+		Uploader:    "",
+		PrepTime:    time.Hour + time.Minute,
+		TotalTime:   time.Hour + time.Minute,
+		Yield:       "",
+		Ingredients: "",
+		Image:       "",
+		Steps:       "",
+	}
+
+	if err := editTpl.Execute(w, data); err != nil {
 		log.Printf("Failed to execute createGet %v\n", err)
 		http.Error(w, "server error", http.StatusInternalServerError)
 	}
+}
+
+func (router *Router) createPostRecipeHandler(w http.ResponseWriter, r *http.Request) {
+	dbData, err := fillDataFromForm(r)
+	if err != nil {
+		http.Error(w, "Invalid form data", http.StatusBadRequest)
+		return
+	}
+
+	// Send to DB
+	builder := internal.TestRecipeDataStrategy{}
+	id, err := builder.CreateRecipe(dbData)
+	if err != nil {
+		http.Error(w, "Could not create recipe", http.StatusInternalServerError)
+		return
+	}
+
+	// Reroute to the new read page for the created index
+	http.Redirect(w, r, fmt.Sprintf("/read/%d", id), http.StatusSeeOther)
 }
